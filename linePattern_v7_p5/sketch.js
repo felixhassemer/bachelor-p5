c99beaf7a893e793f85473cb1380d114c6d4d71a
Felix Hassemer <felixhassemer@gmail.com>
2017-01-10 17:42:09 +0100

    implemented basic functionality

--- linePattern_v7_p5/sketch.js
+++ linePattern_v7_p5/sketch.js
@@ -1,25 +1,56 @@
 // VARIABLES
-var unit = {
-  uWmin: 10,
-  uWmax: 140,
-  uHmin: 10,
-  uHmax: 160,
-  uW: 0,
-  uH: 0
+var u = {   // UNITS
+  wMin: 10,
+  wMax: 140,
+  hMin: 10,
+  hMax: 160,
+  w: 0,
+  h: 0
 }
 
-var pos = {
+var choose = {
+  main: 0,
+  pattern: 0
+}
+
+var patterns = [
+  this[0] = {
+    name: "diagLine",
+    value: 0
+  }
+
+]
+
+var a = {   // ARRAY
+  rOff: [10], // random offset
+  key: [],
+  val: []
+}
+
+var n = {   // NOISE
+  x : {
+    off: 0,
+    incr: 0.01
+  },
+  y : {
+    off: 0,
+    incr: 0.05
+  }
+}
+
+var p = { // POSITION
   x: 0,
-  y: 0,
+  y: 0
 }
 
-var padding = 100;
+var border = 0;
+var bgndColor = 255;
 
-var f = {   // fill
-  color: 0,
-};
+var f = {   // FILL
+  color: 0
+}
 
-var s = {   // stroke
+var s = {   // STROKE
   color: 0,
   weight: 3,
   off: 0,
@@ -34,16 +65,104 @@ function setup() {
   s.cap = ROUND;
   s.join = ROUND;
 
-  unit.uW = random(unit.uWmin, unit.uWmax);
-  unit.uH = random(unit.uHmin, unit.uHmax);
+  u.w = random(u.wMin, u.wMax);
+  u.h = random(u.hMin, u.hMax);
 
-  console.log(unit.uW, unit.uH);
+  console.log(patterns[0]);
 
+  // INIT ARRAY
+  for (var i=0; i< 10; i++) {
+    a.rOff[i] = random(100000);
+  }
+
+  setPatternNoise();
+
+  // SETTINGS
   createCanvas(800,800);
   frameRate(25);
-  background(0);
+  background(bgndColor);
 }
 
 function draw() {
+  // choose pattern
+  choose.main = map(noise(n.x.off), 0, 1, 0, 100);
 
+  clearSides();
+
+  // set Unit Width to random or until margin
+  if (dist(p.x, p.y, width - border*2, p.y) > u.wMax) {
+    u.w = round(random(u.wMin, u.wMax));
+  } else {
+    u.w = dist(p.x, p.y, width - border*2, p.y);
+  }
+
+  diagLine();
+
+  // Paragraph Overflow or move X
+  if (p.x + u.w > width - border*2-1) {
+    // random Unit Height
+    p.x = 0;
+    p.y += u.h;
+    u.h = round(random(u.hMin, u.hMax));
+  } else {
+    p.x += u.w;
+  }
+
+  // NOISE increment
+  n.x.off += n.x.incr;
+  n.y.off += n.y.incr;
+
+  scrollScreen();
+}
+
+// ----------------------------------------------------------
+// CORE FUNCTIONS
+
+function chooseFunction() {
+  // for (var i=0; i< patterns.length)
+}
+
+function setPatternNoise() {
+  patterns[0] = {
+    "name": "diagLine",
+    "keyValue": map(random(n.y.off + a.rOff[0]), 0, 1, 0, 100)
+  }
+}
+
+function scrollScreen() {
+  if (p.y + u.h >= height) {
+    copy(this, 0, 0, width, p.y, 0, -u.h, width, p.y);
+    fill(bgndColor);
+    noStroke();
+    rect(0 - border, p.y - u.h, width, u.hMax);
+    p.y = p.y - int(u.h);
+  }
+}
+
+function clearSides() {
+  fill(bgndColor);
+  noStroke();
+  rect(0, 0, border, p.y);
+  rect(width - border, 0, border, p.y);
+  translate(border, 0);
+}
+
+// ----------------------------------------------------------
+// PATTERN FUNCTIONS
+
+function diagLine() {
+  // STYLING
+  stroke(s.color);
+  strokeWeight(s.weight);
+  strokeCap(s.cap);
+  strokeJoin(s.join);
+  noFill();
+
+  // PATTERN
+  choose.pattern = round(random(1));
+  if (choose.pattern == 0) {
+    line(p.x, p.y+s.off, p.x+u.w, p.y+u.h-s.off);
+  } else {
+    line(p.x, p.y+u.h-s.off, p.x+u.w, p.y+s.off);
+  }
 }
