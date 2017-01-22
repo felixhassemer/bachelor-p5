243c24592e36b892a4fcfdc0366d709f69ac4c61
Felix Hassemer <felixhassemer@gmail.com>
2017-01-22 18:27:45 +0100

    bugfixing

--- mondrian/sketch.js
+++ mondrian/sketch.js
@@ -1,22 +1,46 @@
+var canvasW, canvasH;
+var rect1, rect2, rect3;
+
+
+
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  // calculate canvassize
+  canvasW = windowWidth-windowWidth/4;
+  canvasH = windowWidth-windowWidth/4;
+
+  // rectangle sizes
+  rect1 = canvasW/2;
+  rect2 = canvasW/3;
+  rect3 = canvasW/2;
+
+  createCanvas(canvasW, canvasH);
   background(255);
   fill(255);
   noStroke();
-  frameRate(5);
+  frameRate(4);
   rectMode(CENTER);
 }
 
 function draw() {
-  fill(255);
-  rect(random(windowWidth), random(windowHeight), random(600), random(600));
+  // draw the rectangles
+  fill(240);
+  rect(random(canvasW), random(canvasH), random(rect1), random(rect1));
   fill(190, 90, 90);
-  rect(random(windowWidth), random(windowHeight), random(400), random(400));
+  rect(random(canvasW), random(canvasH), random(rect2), random(rect2));
   fill(40);
-  rect(random(windowWidth), random(windowHeight), random(600), random(600));
+  rect(random(canvasW), random(canvasH), random(rect3), random(rect3));
 }
 
 function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
+    // calculate canvassize
+  canvasW = windowWidth-windowWidth/4;
+  canvasH = windowWidth-windowWidth/4;
+
+  resizeCanvas(canvasW, canvasH);
+
+  // rectangle sizes
+  rect1 = canvasW/2;
+  rect2 = canvasW/3;
+  rect3 = canvasW/2;
   background(255);
 }
