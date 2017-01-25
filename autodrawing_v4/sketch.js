532321dda648642cbcd66f27673ee97200a6fe7c
Felix Hassemer <felixhassemer@gmail.com>
2017-01-25 22:44:29 +0100

    object variables and functions

--- autodrawing_v4/sketch.js
+++ autodrawing_v4/sketch.js
@@ -1,21 +1,8 @@
+"use strict";
+
 // GLOBAL variables
 
-var song;
-var amp;
-var vol;
-
-var x = {
-  pos: 0,
-  target: 0
-}
-var y = {
-  pos: 0,
-  target: 0
-}
-
-var easing = 0.05;
-var border = 40;
-var objSize = 4;
+var smoothCorner = 5;
 
 var col = {
   bgnd: 0,
@@ -23,81 +10,100 @@ var col = {
   s: 0
 }
 
-var bpm = 128;
 var fr = 60;
-var factor = fr/bpm;
+
+// s is sound
+var s = {
+  bpm: 120,
+  song: null,
+  amp: null,
+  vol: null,
+  // calculate the duration of one beat in frames
+  oneBeat: function() {
+    return round(fr*(fr/s.bpm));
+  }
+}
+
 
 // ----------------------------------------------------------
 
 function preload() {
+  s.song = loadSound("data/everythingbefore.mp3");
 }
 
 function setup() {
   createCanvas(800, 800);
   background(col.bgnd);
   frameRate(fr);
-  song = loadSound("data/everythingbefore.mp3", loaded);
-  amp = new p5.Amplitude();
-  amp.toggleNormalize(1);
+
+  s.song.play();
+  s.amp = new p5.Amplitude(0.1);
+  s.amp.toggleNormalize(1);
 
   stroke(col.s);
   strokeWeight(1);
   rectMode(CENTER);
   fill(col.f);
-  // noFill();
 }
 
 function draw() {
   translate(canvas.width/2, canvas.height/2);
 
-  vol = amp.getLevel();
-  console.log(vol);
-  objSize = map(vol, 0, 1, 0, 180);
+  s.vol = s.amp.getLevel();
+  obj.size = map(s.vol, 0, 1, 0, 180);
 
-  // calculate Target point
-  generateTarget();
-
-  // clear screen every 4 beats
-  clearScreen();
+  // calculate Target point setTarget(border)
+  obj.setTarget(80);
 
   // easing
-  x.pos += (x.target-x.pos) * easing;
-  y.pos += (y.target-y.pos) * easing;
+  obj.ease(0.1);
 
-  // draw mirrored shapes
-  generate(1);
-  generate(-1);
+  // draw mirrored shape
+  obj.show(1);
+  obj.show(-1);
+
+  // clear screen every 4 beats
+  clearScreen(4);
 }
 
 // ----------------------------------------------------------
 
-function generate(mirror) {
-  if ((mirror === undefined) || (mirror != -1)) {
-    mirror = 1;
-  }
-  if (vol < 0.2) {
-    ellipse(mirror*x.pos, mirror*y.pos, 5, 5);
-  } else if (vol < 0.8) {
-    ellipse(mirror*x.pos, mirror*y.pos, objSize, objSize);
-  } else {
-    rect(mirror*x.pos, mirror*y.pos, objSize, objSize);
+var obj = {
+  xPos: 0,
+  yPos: 0,
+  xTarget: 0,
+  yTarget: 0,
+  size: 0,
 
+  ease: function(easing) {
+    obj.xPos += (obj.xTarget-obj.xPos) * easing;
+    obj.yPos += (obj.yTarget-obj.yPos) * easing;
+  },
+
+  show: function(mirror) {
+    if ((mirror === undefined) || (mirror != -1)) {
+      mirror = 1;
+    }
+    if (s.vol < 0.2) {
+      ellipse(mirror*obj.xPos, mirror*obj.yPos, 5, 5);
+    } else if (s.vol < 0.7) {
+      ellipse(mirror*obj.xPos, mirror*obj.yPos, obj.size, obj.size);
+    } else {
+      rect(mirror*obj.xPos, mirror*obj.yPos, obj.size, obj.size, smoothCorner);
+    }
+  },
+
+  setTarget: function(border) {
+    if (frameCount % s.oneBeat() == 1) {
+      obj.xTarget = random(-canvas.width/2+border, canvas.width/2-border);
+      obj.yTarget = random(-canvas.height/2+border, canvas.height/2-border);
+    }
   }
+
 }
 
-function generateTarget() {
-  if (frameCount % round(fr*factor) == 1) {
-    x.target = random(-canvas.width/2+border, canvas.width/2-border);
-    y.target = random(-canvas.height/2+border, canvas.height/2-border);
-  }
-}
-
-function clearScreen() {
-  if (frameCount % round(((fr*factor)*4)) == 0) {
+function clearScreen(beats) {
+  if (frameCount % (s.oneBeat()*beats) == 0) {
     background(col.bgnd);
   }
-}
-
-function loaded() {
-  song.play();
 }
