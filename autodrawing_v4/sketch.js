454583fa4097c9a60ae0064075bb8db12f9ad4b8
Felix Hassemer <felixhassemer@gmail.com>
2017-01-25 19:45:19 +0100

    added songinput

--- autodrawing_v4/sketch.js
+++ autodrawing_v4/sketch.js
@@ -1,5 +1,9 @@
 // GLOBAL variables
 
+var song;
+var amp;
+var vol;
+
 var x = {
   pos: 0,
   target: 0
@@ -10,8 +14,8 @@ var y = {
 }
 
 var easing = 0.05;
-var border = 50;
-var circleSize = 4;
+var border = 40;
+var objSize = 4;
 
 var col = {
   bgnd: 0,
@@ -25,10 +29,17 @@ var factor = fr/bpm;
 
 // ----------------------------------------------------------
 
+function preload() {
+}
+
 function setup() {
   createCanvas(800, 800);
-  background(0);
+  background(col.bgnd);
   frameRate(fr);
+  song = loadSound("data/Igloo-Dentelle.mp3", loaded);
+  amp = new p5.Amplitude();
+  amp.toggleNormalize(1);
+
 
   stroke(255);
   strokeWeight(1);
@@ -39,8 +50,8 @@ function setup() {
 function draw() {
   translate(canvas.width/2, canvas.height/2);
 
-  // vol = amp.getLevel();
-  circleSize = 100;
+  vol = amp.getLevel();
+  objSize = map(vol, 0, 1, 0, 200);
 
   // calculate Target point
   generateTarget();
@@ -63,7 +74,7 @@ function generate(mirror) {
   if ((mirror === undefined) || (mirror != -1)) {
     mirror = 1;
   }
-  ellipse(mirror*x.pos, mirror*y.pos, circleSize, circleSize);
+  ellipse(mirror*x.pos, mirror*y.pos, objSize, objSize);
 }
 
 function generateTarget() {
