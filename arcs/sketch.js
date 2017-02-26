a59cd2011e65231f5d8848c3792a32c08dd343d5
Felix Hassemer <felixhassemer@gmail.com>
2017-02-26 20:39:37 +0100

    modified for saveCanvas

--- arcs/sketch.js
+++ arcs/sketch.js
@@ -1,37 +1,65 @@
-var border = 200;
+var border = 300;
+var sWeight = 10;
+
+var arcs = {
+  space: null,
+  diam: null
+}
+
+var w = {
+  width: 4200,
+  height: 4200
+}
+
+var col = {
+  bgnd: 0,
+  s: 255
+}
+
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  background(153, 37, 68);
+  arcs.diam = w.width / 10;
+  arcs.space = w.width / 13;
+  // createCanvas(windowWidth, windowHeight);
+  // background(153, 37, 68);
+  createCanvas(w.width, w.height);
+  background(col.bgnd);
   frameRate(60);
   colorMode(RGB);
   noFill();
-  stroke(47, 214, 169);
-  strokeWeight(3);
+  stroke(col.s);
+  // stroke(47, 214, 169);
+  strokeWeight(sWeight);
 }
 
 function draw() {
-  background(153, 37, 68);
+  background(col.bgnd);
 
-  var arcStart = radians(map(mouseX, 0, windowWidth, 0, 360));
-  var arcStop = radians(map(mouseY, 0, windowHeight, 0, 360));
+  var arcStart = radians(map(mouseX, 0, w.width, 0, 360));
+  var arcStop = radians(map(mouseY, 0, w.height, 0, 360));
 
   if (arcStart < arcStop) {
-    for (let x = border; x < windowWidth-border; x+=100) {
-      for (let y = border; y < windowHeight-border; y+=100) {
-        arc(x, y, 130, 130, arcStart, arcStop, OPEN);
+    for (let x = border; x < w.width-border; x+=arcs.space) {
+      for (let y = border; y < w.height-border; y+=arcs.space) {
+        arc(x, y, arcs.diam, arcs.diam, arcStart, arcStop, OPEN);
       }
     }
   }
   if (arcStart > arcStop) {
-    for (let x = border; x < windowWidth-border; x+=100) {
-      for (let y = border; y < windowHeight-border; y+=100) {
-        arc(x, y, 130, 130, arcStop, arcStart, OPEN);
+    for (let x = border; x < w.width-border; x+=arcs.space) {
+      for (let y = border; y < w.height-border; y+=arcs.space) {
+        arc(x, y, arcs.diam, arcs.diam, arcStop, arcStart, OPEN);
       }
     }
   }
 }
 
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
+// function windowResized() {
+//   resizeCanvas(windowWidth, windowHeight);
+// }
+
+function keyTyped() {
+  if (key === 's') {
+    saveCanvas('floweroflife', 'png');
+  }
 }
